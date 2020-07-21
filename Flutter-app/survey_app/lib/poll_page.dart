eb878825cdf0e152b8de0a7a5541b8c139f8096e
Enes <ensmrdane@gmail.com>
2020-07-21 19:28:26 +0300

    Same commit
    same commit

--- Flutter-app/survey_app/lib/poll_page.dart
+++ Flutter-app/survey_app/lib/poll_page.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
-import 'custom_appbar.dart';
+import 'custom_appbar2.dart';
 
 class PollPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Material(
-      // Column is a vertical, linear layout.
-      child: Column(
-        children: <Widget>[
-          MyAppBar(
-            title: Text(
-              'Corona Survey App',
-              style: Theme.of(context).primaryTextTheme.headline6,
-            ),
-          ),
-          MyCustomForm(),
-        ],
-      ),
+    return Scaffold(
+        appBar: CustomAppBar(),
+        body: Material(
+            child: Column(
+                children: <Widget>[
+                  MyCustomForm()
+                ]
+            )
+        )
     );
   }
 }
